f57e570f7d994133b385368232c1f9334272f6a9
godwin-SM <godwin.ai25@stellamaryscoe.edu.in>
2026-03-13 23:28:14 +0530

    new

--- app.py
+++ app.py
@@ -209,4 +209,5 @@ async def upload(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
