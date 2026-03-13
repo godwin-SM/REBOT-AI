5b9438d06f8252624023e71162bdf92728891902
godwin-SM <godwin.ai25@stellamaryscoe.edu.in>
2026-03-13 19:58:52 +0530

    port

--- app.py
+++ app.py
@@ -203,13 +203,3 @@ async def upload(file: UploadFile = File(...)):
     return {"reply": reply}
 
 
-# -----------------------
-# RUN SERVER
-# -----------------------
-if __name__ == "__main__":
-    import uvicorn
-    import os
-
-    port = int(os.environ.get("PORT", 10000))
-
-    uvicorn.run(app, host="0.0.0.0", port=port)
